e86a53d524a8c53dd69cc82f74b63349dae48f60
JasoDing <dingzihaojason@gmail.com>
2025-05-31 09:46:26 -0400

    fixed comment format for JsonToCSV.cpp

--- utils/JsonCsvTraceConvert/JsonToCSV.cpp
+++ utils/JsonCsvTraceConvert/JsonToCSV.cpp
@@ -1,8 +1,8 @@
-# SPDX-FileCopyrightText: 2025 Systems and Multimedia Lab @ Rutgers University
-# SPDX-License-Identifier: Apache-2.0
-#
-# This file is part of the 👁️NavGS (EyeNavGS) project.
-# See LICENSE_EYENAVGS.txt and NOTICE for details.
+// SPDX-FileCopyrightText: 2025 Systems and Multimedia Lab @ Rutgers University
+// SPDX-License-Identifier: Apache-2.0
+//
+// This file is part of the 👁️NavGS (EyeNavGS) project.
+// See LICENSE_EYENAVGS.txt and NOTICE for details.
 
 
 #include <iostream>
